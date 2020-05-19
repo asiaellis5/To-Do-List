81b73f5fad010cecf41fcf73fb967e1aa7509b7b
Asia <asiaellis5@gmail.com>
2020-05-19 17:03:32 +0100

    added to get requests to get all the todos and get one todo

--- server/index.js
+++ server/index.js
@@ -20,15 +20,46 @@ app.post("/todos", async (req, res) => {
 
     res.json(newTodo.rows);
   } catch (error) {
-    console.log(error)
+    console.error(error.message)
   }
 })
 
 // get all todos
 
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo;")
+
+    res.json(allTodos.rows)
+
+  } catch (error) {
+    console.error(error.message)
+  }
+})
+
 // get a todo
 
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+
+    res.json(todo.rows[0])
+
+  } catch (error) {
+    console.error(error.messsage)
+  }
+})
+
 //update a todo
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+
+  } catch (error) {
+    console.error(error.messsage)
+  }
+})
 
 // delete a todo
 
