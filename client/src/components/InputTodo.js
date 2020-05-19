03859b968388fc80aca256423c0c4d21a78f5ad0
Asia <asiaellis5@gmail.com>
2020-05-19 17:36:42 +0100

    added state to input todo

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,12 +1,18 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
+  const [description, setDescription] = useState("");
   return (
     <Fragment>
       {" "}
       <h1 className="text-center mt-5">Todo List</h1>
       <form className="d-flex mt-5">
-        <input type="text" className="form-control" />
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button className="btn btn-success">Add</button>
       </form>
     </Fragment>
