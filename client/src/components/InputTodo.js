8e41fcccbede818110ecbb06ddad9b43a89e44c2
Asia <asiaellis5@gmail.com>
2020-05-20 09:22:23 +0100

    refactored slightly and fixed some typos

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -13,7 +13,7 @@ const InputTodo = () => {
         body: JSON.stringify(body),
       });
 
-      window.location("/");
+      window.location = "/";
     } catch (error) {
       console.error(error.message);
     }
